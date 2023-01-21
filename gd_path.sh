83d853bd7291572d7af6c02384035d48aa6c1059
matt-rw <85706829+matt-rw@users.noreply.github.com>
2023-01-21 11:23:31 -0500

    Add files via upload

---
+++ gd_path.sh
@@ -0,0 +1,3 @@
+echo 'export PATH=/Library/Frameworks/Python.framework/Versions/3.10/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:"/Users/matthewwear/Documents/Software/Other/code/umusi/geckodriver"' >> ~/.bash_profile
+source ~./bash_profile
+
